862126c2044c318a44e3ccd32908a444f98c9b4c
Dimyati Utoyo <didim.utoyo@gmail.com>
2019-05-25 17:49:44 +0700

    fix add skill

--- 6b.php
+++ 6b.php
@@ -25,10 +25,10 @@
         </div>
         <div class="row">
             <div class="span12">
-                <form id="form-user" class="well form-inline">
+                <div id="form-user" class="well form-inline">
                     <input type="text" id="user" class="span4">
                     <button onclick="addUser()" class="btn">Tambah</button>
-                </form>
+                </div>
             </div>
         </div>
         <div class="row">
@@ -50,3 +50,83 @@
 </html>
 
 <script src="bootstrap/js/jquery.js"></script>
+<script>
+    $(document).ready(function () {
+        $.ajax('ajax/get_data.php', {
+            dataType: 'json',
+            success: function (data) {
+                writeTable(data);
+            }
+        });
+    });
+
+    function addUser() {
+        // e.preventDefault();
+
+        user = $('#user').val();
+
+        $.ajax('ajax/insert_user.php', {
+            data: { user: user },
+            method: 'post',
+            dataType: 'json',
+            success: function (data) {
+                if (data.status == 'success') {
+                    writeTable(data);
+                }
+                else if (data.status == 'failed') {
+                    window.setTimeout(function () {
+                        $(".alert").fadeTo(500, 0).slideUp(500, function () {
+                            $(this).remove();
+                        });
+                    }, 4000);
+                    $('#notif-tambah').html('<div class="alert alert-error">' + data.message + '</div>');
+                }
+            }
+        });
+    }
+
+    function addSkill(id) {
+        if($('#skill'+id).val() == ''){
+            $('#skill'+id).focus();
+        }
+        skill = $('#skill'+id).val();
+
+        $.ajax('ajax/insert_skill.php', {
+            data: { skill: skill, id: id },
+            method: 'post',
+            dataType: 'json',
+            success: function (data) {
+                if (data.status == 'success') {
+                    writeTable(data);
+                }
+                else if (data.status == 'failed') {
+                    window.setTimeout(function () {
+                        $(".alert").fadeTo(500, 0).slideUp(500, function () {
+                            $(this).remove();
+                        });
+                    }, 4000);
+                    $('#notif-tambah').html('<div class="alert alert-error">' + data.message + '</div>');
+                }
+            }
+        });
+    }
+
+    function writeTable(data) {
+        console.log(data);
+        $('#tbody').empty();
+        for (i = 0; i < data.data.length; i++) {
+            $('#tbody').append('<tr>'
+                + '<td width="15px" rowspan="2">' + (i + 1) + '</td>'
+                + '<td>' + data.data[i].name + '</td>'
+                + '<td width="30%" rowspan="2">'
+                + ' <div class="form-inline">'
+                + '<input type="text" class="span2" id="skill'+data.data[i].id+'">'
+                + '<button type="submit" onclick="addSkill('+data.data[i].id+')" class="btn">Tambah</button>'
+                + '</div>'
+                + '</td>'
+                + '<tr>'
+                + '<td>' + data.data[i].skill+ '</td>'
+                + '</tr>');
+        }
+    }
+</script>
